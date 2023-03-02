ac3815394def66ffc5185833135749aa14aa5af3
Ido Tanne <itanne@deloitte.com>
2023-03-02 16:18:06 -0500

    Added Project

--- src/Static/Data/Projects.js
+++ src/Static/Data/Projects.js
@@ -1,1 +1,14 @@
-export const MOCK_DATA = [{"UUID":"12a52ddc-f5ad-44d7-9c38-0bab08e632cb","App_Name":"Fake-Person-Generator","App_IMG":"9b5d6de8-65fe-4328-aac9-bf054f62c07c.png","App_URL":"https://fake-person-generator.vercel.app/"},]
+export const MOCK_DATA = [
+  {
+    UUID: "12a52ddc-f5ad-44d7-9c38-0bab08e632cb",
+    App_Name: "Fake-Person-Generator",
+    App_IMG: "9b5d6de8-65fe-4328-aac9-bf054f62c07c.png",
+    App_URL: "https://fake-person-generator.vercel.app/",
+  },
+  {
+    UUID: "12a52ddc-f5ad-44d7-9c38-0eh43f78",
+    App_Name: "Error Handeling",
+    App_IMG: "ac8f5c67-869d-43cf-8b5f-f3ce496983c0.png",
+    App_URL: "https://react-error-handeling-project.vercel.app/",
+  },
+];

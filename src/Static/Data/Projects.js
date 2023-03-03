0154b964c4501d910a4b8532f6a4025eca77cf91
Ido Tanne <itanne@deloitte.com>
2023-03-03 10:29:19 -0500

    added "Sort and Filter" to list

--- src/Static/Data/Projects.js
+++ src/Static/Data/Projects.js
@@ -1,14 +1,20 @@
 export const MOCK_DATA = [
   {
-    UUID: "12a52ddc-f5ad-44d7-9c38-0bab08e632cb",
+    UUID: "9b5d6de8-65fe-4328-aac9-bf054f62c07c",
     App_Name: "Fake-Person-Generator",
     App_IMG: "9b5d6de8-65fe-4328-aac9-bf054f62c07c.png",
     App_URL: "https://fake-person-generator.vercel.app/",
   },
   {
-    UUID: "12a52ddc-f5ad-44d7-9c38-0eh43f78",
+    UUID: "ac8f5c67-869d-43cf-8b5f-f3ce496983c0",
     App_Name: "Error Handeling",
     App_IMG: "ac8f5c67-869d-43cf-8b5f-f3ce496983c0.png",
     App_URL: "https://react-error-handeling-project.vercel.app/",
+  },
+  {
+    UUID: "12a52ddc-f5ad-44d7-9c38-8fh3467w",
+    App_Name: "Sort and Filter",
+    App_IMG: "12a52ddc-f5ad-44d7-9c38-8fh3467w.png",
+    App_URL: "https://react-sort-filter-seven.vercel.app/",
   },
 ];
